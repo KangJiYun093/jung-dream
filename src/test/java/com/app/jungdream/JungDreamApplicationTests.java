1b5d8b0f5b98d14ad9e70273ae27bccaab5c5ff5
DESKTOP-UQKKKKH\CHJ <tonky0810@naver.com>
2024-01-17 10:54:05 +0900

    초기 생성

---
+++ src/test/java/com/app/jungdream/JungDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.jungdream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
