e6be23957cb2f2ea15407e5d737061ddf6ad4b91
KangJiYun093 <regina091373@gmail.com>
2024-02-10 14:15:59 +0900

    - 정현진 평생 솔로 ㅅㄱ

---
+++ src/main/resources/static/js/customer/cust-announce.js
@@ -0,0 +1,23 @@
+//공지사항
+$(() => {
+    $("#tit-anno, #tit-center").on("click", () => {
+        console.log("Click event triggered");
+        location.href = "../customer/cust-announce.html";
+    });
+});
+
+//1:1문의 작성 폼
+$(() => {
+    $("#tit-inForm").on("click", () => {
+        console.log("Click event triggered");
+        location.href = "../customer/cust-inquiry-form.html";
+    });
+});
+
+//1:1문의내역
+$(() => {
+    $("#tit-inForm-list").on("click", () => {
+        console.log("Click event triggered");
+        location.href = "../customer/cust-inquiry-list.html";
+    });
+});
