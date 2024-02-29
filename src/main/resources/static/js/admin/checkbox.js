0f73db5338c5c7127b2d4cb757809f0be7384ff1
dev-sein <seinee114@gmail.com>
2024-02-29 16:30:50 +0900

    관리자 수정/체크박스/수정

--- src/main/resources/static/js/admin/checkbox.js
+++ src/main/resources/static/js/admin/checkbox.js
@@ -1,25 +1,25 @@
 // 체크박스
-const selectAll = document.querySelector('#select-all');
-const selectMembers = document.querySelectorAll('.select-member');
-
-selectAll.addEventListener('click', (e) => {
-    selectMembers.forEach(member => member.checked = e.target.checked);
+ /* 전체 선택을 눌렀을 때 */
+ $('#all-check').on('click', function() {
+    if(!$(this).is(':checked')) {
+        console.log("체크 되어있음")
+        $('input:checkbox').prop('checked', false);
+        $(this).prop('checked', false);
+    } else {
+        console.log("체크 안 되어있음")
+        $('input:checkbox').prop('checked', true);
+        $(this).prop('checked', true);
+    }
 });
 
-selectMembers.forEach(member => {
-    member.addEventListener('click', (e) => {
-        console.log(document.querySelectorAll('.select-member:checked').length);
-        console.log(selectMembers.length);
-        if (!e.target.checked) {
-            selectAll.checked = false;
-        } else if (document.querySelectorAll('.select-member:checked').length === selectMembers.length) {
-            selectAll.checked = true;
-        }
-    });
-});
+/* 개별 선택할때 전체 선택 처리 */
+$("input[name=check]").click(function() {
+    var total = $("input[name=check]").length;
+    var checked = $("input[name=check]:checked").length;
 
-// 스크롤 기능 막기
-// window.addEventListener("scroll", () => {
-//     window.scrollTo(0, 0);
-// });
-// 스크롤 기능 막기
+    if(total != checked) {
+        $(".all-check").prop("checked", false);
+    } else {
+        $(".all-check").prop("checked", true);
+    }
+});
