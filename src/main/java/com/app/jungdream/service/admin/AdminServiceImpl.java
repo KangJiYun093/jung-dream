456ad509dae2088bb17814f54e3b215f4501febe
DESKTOP-UQKKKKH\CHJ <tonky0810@naver.com>
2024-03-20 16:53:43 +0900

    품종관리 리스트 불러오기 및 수정 완료

--- src/main/java/com/app/jungdream/service/admin/AdminServiceImpl.java
+++ src/main/java/com/app/jungdream/service/admin/AdminServiceImpl.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -14,7 +16,22 @@ public class AdminServiceImpl implements AdminService {
     private final ProductRegistrationDAO productRegistrationDAO;
 
     @Override
+    public List<ProductRegistrationVO> getAllProductRegistration() {
+        return productRegistrationDAO.selectAllProductRegistration();
+    }
+
+    @Override
+    public ProductRegistrationVO getProductRegistration(Long productRegistrationId) {
+        return productRegistrationDAO.selectProductRegistration(productRegistrationId);
+    }
+
+    @Override
     public void saveRegistration(ProductRegistrationVO productRegistrationVO) {
         productRegistrationDAO.insertProductRegistration(productRegistrationVO);
+    }
+
+    @Override
+    public void editRegistration(ProductRegistrationVO productRegistrationVO) {
+        productRegistrationDAO.updateProductRegistration(productRegistrationVO);
     }
 }
--- src/main/resources/static/js/admin/kind.js
+++ src/main/resources/static/js/admin/kind.js
@@ -1,11 +1,77 @@
 $(function() {
+
+    productRegistrations.forEach(productRegistration => {
+        let text = `
+            <tr class="registration-list">
+                <input type="hidden" value="${productRegistration.productRegistrationId}" id="product-registration-id">
+                <td><input type="checkbox" name="check"></td>
+                <td>${productRegistration.productRegistrationName}</td>
+                <td>${productRegistration.productRegistrationSpecification}</td>
+                <td>${productRegistration.productRegistrationWeight}</td>
+                <td>${productRegistration.productRegistrationQuantity}</td>
+                <td>${productRegistration.productRegistrationPrice}</td>
+            </tr>
+        `;
+
+        $('.kind-content').append(text);
+    });
+
+    $('.registration-list').on('click', function() {
+        $productRegistrationId = $(this).find('#product-registration-id').val();
+
+        $.ajax({
+            url: "/admins/kind/get-registration",
+            type: "post",
+            data: { productRegistrationId : $productRegistrationId },
+            success: function(result) {
+                if(result) {
+                    $('#edit-modal').find('input[name=productRegistrationId]').val(result.productRegistrationId);
+                    $('#edit-modal').find('input[name=productRegistrationName]').val(result.productRegistrationName);
+
+                    if(result.productRegistrationSpecification == 'Y') {
+                        $('#edit-modal').find('input[name=productRegistrationSpecification]').prop('checked', true);
+                    } else {
+                        $('#edit-modal').find('input[name=productRegistrationSpecification]').prop('checked', false);
+                    }
+                    if(result.productRegistrationWeight == 'Y') {
+                        $('#edit-modal').find('input[name=productRegistrationWeight]').prop('checked', true);
+                    } else {
+                        $('#edit-modal').find('input[name=productRegistrationWeight]').prop('checked', false);
+                    }
+                    if(result.productRegistrationQuantity == 'Y') {
+                        $('#edit-modal').find('input[name=productRegistrationQuantity]').prop('checked', true);
+                    } else {
+                        $('#edit-modal').find('input[name=productRegistrationQuantity]').prop('checked', false);
+                    }
+                    if(result.productRegistrationPrice == 'Y') {
+                        $('#edit-modal').find('input[name=productRegistrationPrice]').prop('checked', true);
+                    } else {
+                        $('#edit-modal').find('input[name=productRegistrationPrice]').prop('checked', false);
+                    }
+
+                    $('#edit-modal').show();
+                }
+            }
+        });
+    });
+
     $('#insert-button').on('click', () => {
 
-        if(!$('input[name=productRegistrationName]').val()) {
+        if(!$('#insert-name').val()) {
             alert('품종 이름을 입력하세요');
             return;
         }
 
         document.productRegistrationInsertForm.submit();
+    });
+
+    $('#edit-button').on('click', () => {
+
+        if(!$('#update-name').val()) {
+            alert('품종 이름을 입력하세요');
+            return;
+        }
+
+        document.productRegistrationUpdateForm.submit();
     });
 });
