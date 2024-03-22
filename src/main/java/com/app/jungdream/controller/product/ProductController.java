d8305dc430636e4b293bb84fd91a6f0bfd770d8a
dev-sein <seinee114@gmail.com>
2024-03-22 14:55:56 +0900

    세인 커밋

--- src/main/java/com/app/jungdream/controller/product/ProductController.java
+++ src/main/java/com/app/jungdream/controller/product/ProductController.java
@@ -1,8 +1,10 @@
 package com.app.jungdream.controller.product;
 
+import com.app.jungdream.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
+
+    private final ProductService productService;
+
 }
