1476731b88484d6e64afc151c77abd93bb28d9a1
jeffy1217 <younjune1217@gmail.com>
2024-04-07 19:26:55 +0900

    main 작업 중

--- src/main/java/com/app/jungdream/controller/MainController.java
+++ src/main/java/com/app/jungdream/controller/MainController.java
@@ -1,20 +1,37 @@
 package com.app.jungdream.controller;
 
+import com.app.jungdream.domain.vo.CartVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/*")
+@RequestMapping("/main")
 @RequiredArgsConstructor
 @Slf4j
 public class MainController {
 
-        @GetMapping("/main")
-        public String mainpage(){
-            return "main/main";
-        }
+        //    헤더쪽 장바구니
+        @GetMapping("cart")
+        public String moveCart(){
+                List<CartVO> cartVOS
+
+
+                return "/cart/cart";}
+
+        //    헤더쪽 문의하기
+        @GetMapping("inquiry")
+        public String moveInquiry(){ return "/customer/cust-inquiry-list";}
+
+        //     헤더쪽 공지사항
+        @GetMapping("notice")
+        public String moveNotice(){ return "/customer/cust-notice";}
+
+
+
 
 }
