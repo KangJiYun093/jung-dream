a6636be3c1b8cb09c203ca30718da83e7912ab77
jeffy1217 <younjune1217@gmail.com>
2024-05-18 17:44:04 +0900

    CarController, CartMapper, CartService 구현 중

--- src/main/java/com/app/jungdream/controller/cart/CartController.java
+++ src/main/java/com/app/jungdream/controller/cart/CartController.java
@@ -1,32 +1,49 @@
 package com.app.jungdream.controller.cart;
 
 import com.app.jungdream.domain.dto.ProductDTO;
+import com.app.jungdream.domain.vo.ProductVO;
+import com.app.jungdream.service.cart.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/cart/*")
 @RequiredArgsConstructor
 @Slf4j
 public class CartController {
-    private List<ProductDTO> cartItems = new ArrayList<>();
+    private final CartService cartService;
 
-    @PostMapping("/add")
-    public String addItemToCart(@RequestBody ProductDTO product) {
-        cartItems.add(product);
-        return "장바구니에 추가: " + product.getProductTitle();
+    // 페이지로 이동(?)
+    @GetMapping("add")
+    public void add(Model model){
+        model.addAttribute(new ProductVO());
     }
 
-    @GetMapping("/items")
-    public List<ProductDTO> getCartItems() {
-        return cartItems;
+    // 장바구니에 추가
+    @PostMapping("add")
+    public RedirectView add(ProductVO productVO) {
+        cartService.addToCart(productVO);
+        return new RedirectView("/cart/list");
+    }
+
+    // 장바구니에서 상품 삭제
+    @PostMapping("remove")
+    public RedirectView remove(ProductVO productVO, RedirectAttributes redirectAttributes){
+        cartService.removeFromCart(productVO);
+        redirectAttributes.addAttribute("productId", productVO.getProductId());
+        return new RedirectView("/cart/list");
+    }
+
+    // 장바구니 목록 이동
+    @GetMapping("list")
+    public void showList(Model model){
+        model.addAttribute("cartItems", cartService.getCartItems());
     }
 }
--- src/main/java/com/app/jungdream/service/cart/CartService.java
+++ src/main/java/com/app/jungdream/service/cart/CartService.java
@@ -1,10 +1,27 @@
 package com.app.jungdream.service.cart;
 
+import com.app.jungdream.domain.vo.CartVO;
+import com.app.jungdream.domain.vo.ProductVO;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public interface CartService {
+//    private Long cartId;
+//    private Integer cartQuantity;
+//    private String cartCustomerName;
+//    private String cartCustomerPhone;
+//    private Long productId;
 
+    // cart 상품 추가
+    void addToCart(ProductVO productVO);
+
+    // cart 상품 삭제
+    void removeFromCart(ProductVO productVO);
+
+    // 상품 목록 반환(?)
+    List<ProductVO> getCartItems();
 }
